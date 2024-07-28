884ee19c4504a9e7f952d2d6d9087864303bd9c7
Aziz Koumenji <azizkoumenji@protonmail.com>
2024-07-28 14:35:02 +0100

    Removed token when getting comments

--- back/controllers/comments.js
+++ back/controllers/comments.js
@@ -1,21 +1,12 @@
 import db from "../db.js";
-import jwt from "jsonwebtoken";
 
 export const getComments = (req, res) => {
-  const token = req.cookies.accessToken;
+  const q =
+    "select c.*, u.name, u.profile_pic from comments c join users u on c.user_id = u.id where post_id = ?";
 
-  if (!token) return res.status(401).json("User not logged in.");
+  db.query(q, [req.params.id], (err, data) => {
+    if (err) return res.status(500).json(err);
 
-  jwt.verify(token, process.env.SECRET_KEY, (err, userInfo) => {
-    if (err) return res.status(403).json("Invalid token.");
-
-    const q =
-      "select c.*, u.name, u.profile_pic from comments c join users u on c.user_id = u.id where post_id = ?";
-
-    db.query(q, [req.params.id], (err, data) => {
-      if (err) return res.status(500).json(err);
-
-      return res.status(200).json(data);
-    });
+    return res.status(200).json(data);
   });
 };
