6770e43ae7ba8c3ee296fe5a7430188aa64dd68f
Aziz Koumenji <azizkoumenji@protonmail.com>
2024-08-04 18:29:40 +0100

    Some UI changes

--- front/src/components/rightbar/RightBar.jsx
+++ front/src/components/rightbar/RightBar.jsx
@@ -3,35 +3,19 @@ import "./rightbar.scss";
 export default function RightBar() {
   return (
     <div className="rightbar">
-      <div className="suggestions">
-        <span className="title">Suggestions for You</span>
+      <div className="chat">
+        <span className="title">Contacts</span>
         <div className="user">
           <div className="user-info">
-            <img src="" alt="User Image" />
-            <span>User Name</span>
-          </div>
-          <button>Follow</button>
-        </div>
-      </div>
-      <div className="activities">
-        <span className="title">Latest Activities</span>
-        <div className="user">
-          <div className="user-info">
-            <img src="" alt="User Image" />
-            <span>User Name</span>
-            <span className="description">posted an image</span>
-          </div>
-
-          <span className="description">1 min ago</span>
-        </div>
-      </div>
-      <div className="online">
-        <span className="title">Online Friends</span>
-        <div className="user">
-          <div className="user-info">
-            <img src="" alt="User Image" />
+            <img src="/api/uploads/profile.png" alt="User Image" />
             <div className="indicator"></div>
             <span>User Name</span>
+          </div>
+        </div>
+        <div className="user">
+          <div className="user-info">
+            <img src="/api/uploads/profile.png" alt="User Image" />
+            <span>User Name 2</span>
           </div>
         </div>
       </div>
