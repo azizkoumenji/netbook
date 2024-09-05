b05599d4f5947f894f5002400701afba816a35d4
Aziz Koumenji <azizkoumenji@protonmail.com>
2024-09-05 17:01:21 +0100

    Seperated env variables from production and development

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: dotenv.config().parsed.VITE_API_LINK,
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_API_LINK,
+        },
       },
     },
-  },
+  };
 });
