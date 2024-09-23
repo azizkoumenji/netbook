9e8dedf97f346764d0f57ab18377d98fa0539768
Aziz Koumenji <azizkoumenji@protonmail.com>
2024-09-23 16:51:41 +0100

    Added search for mobile version

--- front/src/components/navbar/NavBar.jsx
+++ front/src/components/navbar/NavBar.jsx
@@ -11,6 +11,7 @@ export default function NavBar() {
   const [searchResults, setSearchResults] = useState([]);
   const [searchKey, setSearchKey] = useState("");
   const [showNoResult, setShowNoResult] = useState(false);
+  const [showSearch, setShowSearch] = useState(false);
 
   const handleSearch = async (e) => {
     setSearchKey(e.target.value);
@@ -44,73 +45,145 @@ export default function NavBar() {
   };
 
   return (
-    <div className="navbar">
-      <div className="left">
-        <Link to="/" className="logo">
-          <h1>Netbook</h1>
-        </Link>
-        <Link to="/">
-          <i className="bi bi-house-fill"></i>
-        </Link>
-        {theme === "dark" ? (
-          <i className="bi bi-sun-fill" onClick={toggleTheme}></i>
-        ) : (
-          <i className="bi bi-moon-fill" onClick={toggleTheme}></i>
-        )}
+    <>
+      <div className="navbar">
+        <div className="left">
+          <Link to="/" className="logo">
+            <h1>Netbook</h1>
+          </Link>
+          <Link to="/">
+            <i className="bi bi-house-fill"></i>
+          </Link>
+          {theme === "dark" ? (
+            <i className="bi bi-sun-fill" onClick={toggleTheme}></i>
+          ) : (
+            <i className="bi bi-moon-fill" onClick={toggleTheme}></i>
+          )}
 
-        <Link to="/chat" className="link">
-          <i className="bi bi-chat-fill"></i>
-        </Link>
-        <Link to="/following" className="link">
-          <div className="friends">
-            <i className="bi bi-people-fill"></i>
-          </div>
-        </Link>
-      </div>
-      <div className="search">
-        <i className="bi bi-search"></i>
-        <input
-          type="text"
-          placeholder="Search"
-          onChange={handleSearch}
-          value={searchKey}
-        />
-        {showNoResult ? (
-          <div className="search-results">
-            <span className="no-result">No result</span>
-          </div>
-        ) : (
-          searchResults.length != 0 && (
-            <div className="search-results">
-              {searchResults.map((user) => (
-                <Link
-                  style={{ textDecoration: "none", color: "var(--text-dark)" }}
-                  key={user.id}
-                  to={`/profile/${user.id}`}
-                  onClick={() => {
-                    setSearchKey("");
-                    setSearchResults([]);
-                  }}
-                >
-                  <div className="result">
-                    <img src={user.profile_pic} alt="Profile Picture" />
-                    <span>{user.name}</span>
-                  </div>
-                </Link>
-              ))}
+          <Link to="/chat" className="link">
+            <i className="bi bi-chat-fill"></i>
+          </Link>
+          <Link to="/following" className="link">
+            <div className="friends">
+              <i className="bi bi-people-fill"></i>
             </div>
-          )
-        )}
+          </Link>
+          <i
+            className="bi bi-search search-mobile-icon"
+            onClick={() => {
+              setShowSearch(!showSearch);
+              setSearchKey("");
+              setSearchResults([]);
+              if (document.body.classList.contains("no-scroll")) {
+                document.body.classList.remove("no-scroll");
+              } else {
+                document.body.classList.add("no-scroll");
+              }
+            }}
+          ></i>
+        </div>
+        <div className="search">
+          <i className="bi bi-search"></i>
+          <input
+            type="text"
+            placeholder="Search"
+            onChange={handleSearch}
+            value={searchKey}
+          />
+          {showNoResult ? (
+            <div className="search-results">
+              <span className="no-result">No result</span>
+            </div>
+          ) : (
+            searchResults.length != 0 && (
+              <div className="search-results">
+                {searchResults.map((user) => (
+                  <Link
+                    style={{
+                      textDecoration: "none",
+                      color: "var(--text-dark)",
+                    }}
+                    key={user.id}
+                    to={`/profile/${user.id}`}
+                    onClick={() => {
+                      setSearchKey("");
+                      setSearchResults([]);
+                    }}
+                  >
+                    <div className="result">
+                      <img src={user.profile_pic} alt="Profile Picture" />
+                      <span>{user.name}</span>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            )
+          )}
+        </div>
+        <div className="right">
+          <Link to={`/profile/${currentUser.id}`} className="profile">
+            <img src={currentUser.profile_pic} alt="User Image" />
+            <span>{currentUser.name}</span>
+          </Link>
+          <button onClick={logout}>
+            <i className="bi bi-door-open-fill"></i> Logout
+          </button>
+        </div>
       </div>
-      <div className="right">
-        <Link to={`/profile/${currentUser.id}`} className="profile">
-          <img src={currentUser.profile_pic} alt="User Image" />
-          <span>{currentUser.name}</span>
-        </Link>
-        <button onClick={logout}>
-          <i className="bi bi-door-open-fill"></i> Logout
-        </button>
-      </div>
-    </div>
+      {showSearch && (
+        <div
+          className="search-backdrop"
+          onClick={() => {
+            setShowSearch(!showSearch);
+            setSearchKey("");
+            setSearchResults([]);
+            document.body.classList.remove("no-scroll");
+          }}
+        >
+          <div className="search-mobile" onClick={(e) => e.stopPropagation()}>
+            <div className="search-bar">
+              <i className="bi bi-search"></i>
+              <input
+                type="text"
+                placeholder="Search"
+                onChange={handleSearch}
+                value={searchKey}
+              />
+            </div>
+            {showNoResult ? (
+              <div className="search-results">
+                <span className="no-result">No result</span>
+              </div>
+            ) : (
+              searchResults.length != 0 && (
+                <div className="search-results">
+                  {searchResults.map((user) => (
+                    <Link
+                      style={{
+                        textDecoration: "none",
+                        color: "var(--text-dark)",
+                      }}
+                      key={user.id}
+                      to={`/profile/${user.id}`}
+                      onClick={() => {
+                        setSearchKey("");
+                        setSearchResults([]);
+                        setShowSearch(!showSearch);
+                        document.body.classList.remove("no-scroll");
+                      }}
+                    >
+                      <div className="result">
+                        <img src={user.profile_pic} alt="Profile Picture" />
+                        <span>{user.name}</span>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              )
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
